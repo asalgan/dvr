4d222e15eb2e00f3460b3a22431cb50df8d64794
Alan Salganik <asalganik1@gmail.com>
2014-08-08 17:13:02 -0500

    starting on box selection when recording

--- app/controllers/dvr_systems_controller.rb
+++ app/controllers/dvr_systems_controller.rb
@@ -2,9 +2,10 @@ class DvrSystemsController < ApplicationController
 
   def index
   	@box_1 = Box.find_by(:box_number => 1)
-  	@box_1_shows = @box_1.shows
+  	@box_1_shows = @box_1.shows.where(:recording => true)
+  	
   	@box_2 = Box.find_by(:box_number => 2)
-  	@box_2_shows = @box_2.shows
+  	@box_2_shows = @box_2.shows.where(:recording => true)
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,6 @@ Show.create(title:"The Fox", :channel => 22, :start_time => Time.now, :end_time 
 Show.create(title:"Simpsons", :channel => 37, :start_time => Time.now, :end_time => end_time_half_hour)
 Show.create(title:"Documentary", :channel => 159, :start_time => Time.now, :end_time => end_time_full_hour)
 Show.create(title:"Family Guy", :channel => 12, :start_time => Time.now, :end_time => end_time_full_hour, :single_recording => false)
-Show.create(title:"The League", :channel => 34, :start_time => Time.now, :end_time => end_time_full_hour, :single_recording => false, :box_id => Box.find_by(:box_number => 1).id)
+Show.create(title:"The League", :channel => 34, :start_time => Time.now, :end_time => end_time_full_hour, :single_recording => false)
 Show.create(title:"Goats", :channel => 20, :start_time => Time.now, :end_time => end_time_full_hour, :box_id => Box.find_by(:box_number => 2).id)
 Show.create(title:"Funniest Home Videos", :channel => 45, :start_time => Time.now+205.minutes, :end_time => end_time_full_hour, :box_id => Box.find_by(:box_number => 2).id)
