a2160cdbac1f4f24d01990446c6f292014e72de3
Alan Salganik <asalganik1@gmail.com>
2014-08-09 15:24:08 -0500

    little change

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -31,14 +31,14 @@ class ShowsController < ApplicationController
     current_show_time = show.start_time..show.end_time
 
     if show.box.box_number == 1
-      @current_saved_shows_array_one = []
-      box1_shows = box_one.shows.where(:recording => true)
-
-      box1_shows.each do |showtime|
-        @current_saved_shows_array_one << (showtime.start_time..showtime.end_time)
+      shows = box_one.shows.where(:recording => true)
+      @current_saved_shows_array = []
+      
+      shows.each do |showtime|
+        @current_saved_shows_array << (showtime.start_time..showtime.end_time)
       end
 
-      if @current_saved_shows_array_one.select {|c| current_show_time.overlaps?(c)}.present?
+      if @current_saved_shows_array.select {|c| current_show_time.overlaps?(c)}.present?
         redirect_to show_path(params[:show_id])
       else
         show.update_attributes(:recording => true)
@@ -47,14 +47,14 @@ class ShowsController < ApplicationController
       end
 
     elsif show.box.box_number == 2
-      @current_saved_shows_array_two = []
-      box2_shows = box_two.shows.where(:recording => true)
-
-      box2_shows.each do |showtime|
-        @current_saved_shows_array_two << (showtime.start_time..showtime.end_time)
+      shows = box_two.shows.where(:recording => true)
+      @current_saved_shows_array = []
+      
+      shows.each do |showtime|
+        @current_saved_shows_array << (showtime.start_time..showtime.end_time)
       end
 
-      if @current_saved_shows_array_two.select {|c| current_show_time.overlaps?(c)}.present?
+      if @current_saved_shows_array.select {|c| current_show_time.overlaps?(c)}.present?
         redirect_to show_path(params[:show_id])
       else
         show.update_attributes(:recording => true)
