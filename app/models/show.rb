a47fadeb8ab3182e22d09c6acc4394183d881c88
Alan Salganik <asalganik1@gmail.com>
2014-08-12 16:45:04 -0500

    changed overlapping shows code, can now find which show causes overlap

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,15 +3,13 @@ class Show < ActiveRecord::Base
 
 
   def self.record_show(box, show)
-    shows = box.shows.where(:recording => true)
-    current_show_time = show.start_time..show.end_time
-    @current_saved_shows_array = []
-    
-    shows.each do |showtime|
-      @current_saved_shows_array << (showtime.start_time..showtime.end_time)
-    end
+    overlapping_shows = box.shows.where(recording: true).where("start_time <= :show_end AND end_time >= :show_start", {show_start: show.start_time, show_end: show.end_time}).flatten.first
+    # shows = box.shows.where(:recording => true).flatten
+    # show_start_and_end_times = shows.collect {|x| x.start_time..x.end_time}
+    # current_show_time = show.start_time..show.end_time
+    # overlap = show_start_and_end_times.select {|c| current_show_time.overlaps?(c)}
 
-    if @current_saved_shows_array.select {|c| current_show_time.overlaps?(c)}.present?
+    if overlapping_shows.present?
       nil
     else
       show.update_attributes(:recording => true)
