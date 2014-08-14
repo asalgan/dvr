7695c56805891d1858f861ae218b94977e479d01
Alan Salganik <asalganik1@gmail.com>
2014-08-13 20:53:27 -0500

    application.rb config changes

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*Rails.groups)
 module Dvr
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
+    config.assets.enabled = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
